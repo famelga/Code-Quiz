0e08a5a7ebdaf9c604b883fb124f4faab2d2335a
Fayven <fayven.amelga@gmail.com>
2023-01-04 19:27:58 -0500

    Add function outlines

---
+++ script.js
@@ -0,0 +1,33 @@
+var start=document.getElementById("start")
+
+function begin() {
+    console.log("here")
+    
+  
+}
+
+function countDown() {
+
+}
+
+function questions() {
+
+}
+
+function answers() {
+
+}
+
+function gameOver() {
+
+}
+
+function score() {
+
+}
+
+function saveScore() {
+
+}
+
+start.addEventListener("click", begin)

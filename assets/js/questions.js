b109552517a8f8abf46efce16541cf6d71a97e3c
Fayven <fayven.amelga@gmail.com>
2023-01-05 20:32:53 -0500

    Add questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+        title: "What does html stand for?", 
+        choices: ["World Wide Web", "hyperlink", "HyperText Markup Language", "Javascript"],
+        answer: "HyperText Markup Language"
+    },
+    // {
+    //     title:
+    //     choices: 
+    //     answer:
+    // },
+]

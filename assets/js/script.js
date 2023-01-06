24104a484e03cada8c10bda6000fca64e7bff9fd
Fayven <fayven.amelga@gmail.com>
2023-01-05 20:56:58 -0500

    Add pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,14 @@ var submitEl = document.getElementById("submit");
 var initialsEl = document.getElementById("initials");
 
 function begin() {
-    console.log("here")
+    // hide start
+    // unhide qs
+    // unhide clock
+    // start timer
+    // timerId = setinterval(countDown, 1000)
+    // set text content fron 0 to 60
+    // get a question
+
     // countDown()
     // questions()
     // answers()
@@ -18,21 +25,24 @@ function begin() {
 }
 
 function countDown() {
-    
+    // decrement time by 1; time--
+    // reset text content (timerEl.textcontent = time)
+    // if time<= 0 = execute gameOver function
     
 }
 
-// function questions() {
-//     var answerChoice;
-//     var quest = [{
-//         questions: "What does html stand for?",
-//         answerChoice: ["World Wide Web", "hyperlink", "HyperText Markup Language", "Javascript"]}
-//         // , "Which is not an accessible tag?", "Which language is used for styling?", "Which is an example of an array?", "You need a _______ link for a live site."]
-// }
 
-// function answers() {
+function getQuestion() {
+    var currentQ = questions[questionIndex]
+    // create var TitleEl= document.getElementById("questionTitle")
+    // TitleEl.textcontent= currentQuestion.Title
+    // clear answersEl.innerhtml = "" to reset
+    // for loop to loop over choices and creates button for each....append to answersEl
+}
 
-// }
+function userChoice() {
+    // 
+}
 
 // function gameOver() {
 
